0103a2f3b0745921c77c400fb5598a54b3600e6e
al14085 <asakoboy@gmail.com>
2020-05-20 15:25:36 +0900

    homework5 updated

--- tools/homework5.sh
+++ tools/homework5.sh
@@ -1,3 +1,18 @@
+#for i in material rxjs libimobiledevice ansible
+#do
+#cd ~/workspace/$i
+#git checkout master
+#git pull origin master
+#done
+
+
+#homework5 updated
+
+for n in angular/material ReactiveX/rxjs libimobiledevice/libimobiledevice ansible/ansible
+do
+git clone git@github.com:$n.git
+done
+
 for i in material rxjs libimobiledevice ansible
 do
 cd ~/workspace/$i
